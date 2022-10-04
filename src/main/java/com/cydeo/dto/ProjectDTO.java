8e050a61fe3b1e4b2be8820b69d90f8eade6c4be
lgaid <104643045+lgaid@users.noreply.github.com>
2022-10-04 15:35:12 -0400

    ticketing App

--- src/main/java/com/cydeo/dto/ProjectDTO.java
+++ src/main/java/com/cydeo/dto/ProjectDTO.java
@@ -1,5 +1,4 @@
 package com.cydeo.dto;
-
 import com.cydeo.enums.Status;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,9 +7,10 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
+
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class ProjectDTO {
 
     private String projectName;
@@ -23,6 +23,16 @@ public class ProjectDTO {
     private String projectDetail;
     private Status projectStatus;
 
+    private int completeTaskCounts;
+    private int unfinishedTaskCounts;
 
-
+    public ProjectDTO(String projectName, String projectCode, UserDTO assignedManager, LocalDate startDate, LocalDate endDate, String projectDetail, Status projectStatus) {
+        this.projectName = projectName;
+        this.projectCode = projectCode;
+        this.assignedManager = assignedManager;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.projectDetail = projectDetail;
+        this.projectStatus = projectStatus;
+    }
 }
--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.TaskService;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskServiceImpl extends AbstractMapService<TaskDTO,Long>  implements TaskService {
@@ -48,10 +50,20 @@ public class TaskServiceImpl extends AbstractMapService<TaskDTO,Long>  implement
 
     @Override
     public void update(TaskDTO task) {
+
+        if(task.getTaskStatus() == null)
+            task.setTaskStatus(Status.OPEN);
+        if(task.getAssignedDate() == null)
+            task.setAssignedDate(LocalDate.now());
+
         super.update(task.getId(),task);
     }
 
-
+    public List<TaskDTO> findTasksByManager(UserDTO manager) {
+        return findAll().stream()
+                .filter(task -> task.getProject().getAssignedManager().equals(manager))
+                .collect(Collectors.toList());
+    }
 
 
 
